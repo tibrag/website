a4d5a2c7e76869771058201edf80c2d24634e1aa
evelVT <94784245+evelVT@users.noreply.github.com>
2024-12-12 17:20:41 +0100

    linter fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,27 @@
-console.log('JS is loaded!')
+console.log("JS is loaded!");
 
 // Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu/bar icon
 function mobileNavToggle(how) {
   console.log("Burger toggle: ", how);
   var x = document.getElementsByClassName("nav__menu")[0];
-  switch(how) {
+  switch (how) {
     case "auto":
-      x.classList.toggle("hidden")
+      x.classList.toggle("hidden");
       break;
 
     case "off":
-      x.classList.add("hidden")
+      x.classList.add("hidden");
       break;
 
     case "on":
-      x.classList.remove("hidden")
+      x.classList.remove("hidden");
       break;
   }
 }
 window.mobileNavToggle = mobileNavToggle;
 
-
 let portrait = window.innerWidth <= 640;
-let burger = document.getElementsByClassName('nav__mobile-burger')[0];
+let burger = document.getElementsByClassName("nav__mobile-burger")[0];
 if (window.innerWidth < 640) {
   burger.classList.add("showing");
   mobileNavToggle("off");
@@ -39,61 +38,58 @@ function burgerState() {
     burger.classList.remove("showing");
     //burger.style.display = 'none'; // Hide element in landscape mode
   } else if (!portrait && window.innerWidth <= 640) {
-      portrait = true;
-      console.log("was landscape, now portrait =", portrait);
-      mobileNavToggle("off");
-      burger.classList.add("showing");
-      //burger.style.display = 'absolute'; // Set element to absolute in portrait mode
-      //burger.style.display = 'block'; // Show element in portrait mode
+    portrait = true;
+    console.log("was landscape, now portrait =", portrait);
+    mobileNavToggle("off");
+    burger.classList.add("showing");
+    //burger.style.display = 'absolute'; // Set element to absolute in portrait mode
+    //burger.style.display = 'block'; // Show element in portrait mode
   }
 }
-  
-
-
-
 
 // Throttle function to limit how often function is called
 function throttle(func, limit) {
-	var lastFunc;
-	var lastRan;
-	return function() {
-		var context = this;
-		var args = arguments;
-		if (!lastRan) {
-			func.apply(context, args);
-			lastRan = Date.now();
-		} else {
-			clearTimeout(lastFunc);
-			lastFunc = setTimeout(function() {
-				if ((Date.now() - lastRan) >= limit) {
-					func.apply(context, args);
-					lastRan = Date.now();
-				}
-			}, limit - (Date.now() - lastRan));
-		}
-	};
+  var lastFunc;
+  var lastRan;
+  return function () {
+    var context = this;
+    var args = arguments;
+    if (!lastRan) {
+      func.apply(context, args);
+      lastRan = Date.now();
+    } else {
+      clearTimeout(lastFunc);
+      lastFunc = setTimeout(
+        function () {
+          if (Date.now() - lastRan >= limit) {
+            func.apply(context, args);
+            lastRan = Date.now();
+          }
+        },
+        limit - (Date.now() - lastRan),
+      );
+    }
+  };
 }
 
-var throttledBurgerState = throttle(burgerState, 100)
-window.addEventListener('resize', throttledBurgerState);
-
-
+var throttledBurgerState = throttle(burgerState, 100);
+window.addEventListener("resize", throttledBurgerState);
 
 // darkmode persistent on page change
 const darkModeKey = "darkMode";
 const body = document.body;
-const toggle = document.getElementsByClassName('switch__input')[0];
+const toggle = document.getElementsByClassName("switch__input")[0];
 
 // On load: Apply mode based on localStorage
 if (localStorage.getItem(darkModeKey) === "enabled") {
-    toggle.checked = true;
+  toggle.checked = true;
 }
 
 // Update mode on toggle
 toggle.addEventListener("change", () => {
-    if (toggle.checked) {
-        localStorage.setItem(darkModeKey, "enabled");
-    } else {
-        localStorage.setItem(darkModeKey, "disabled");
-    }
+  if (toggle.checked) {
+    localStorage.setItem(darkModeKey, "enabled");
+  } else {
+    localStorage.setItem(darkModeKey, "disabled");
+  }
 });
